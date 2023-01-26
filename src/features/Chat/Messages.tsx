60cdd42db35cc474659263753f1d5a350b322e58
Brandon Lee <brandonlee2543@hotmail.com>
2023-01-26 14:20:04 -0400

    Added logic to create a Macro timestamp

--- src/features/Chat/Messages.tsx
+++ src/features/Chat/Messages.tsx
@@ -4,12 +4,38 @@ import type { MessageProps } from "../../data/testData";
 import { testMessages } from "../../data/testData";
 
 const Messages = () => {
+  const getTimestamp = (messages: MessageProps[], index: number) => {
+    const currentDate: Date = messages[index]!.message.createdAt;
+    const previousDate =
+      index !== 0 ? messages[index - 1]!.message.createdAt : null;
+
+    if (previousDate) {
+      // TODO: nested if statements 👎🏽 Fix when done, check for timestamp library
+      if (
+        previousDate.getDate() === currentDate.getDate() &&
+        previousDate.getMonth() === currentDate.getMonth() &&
+        previousDate.getFullYear() === previousDate.getFullYear()
+      )
+        return null;
+    }
+
+    return new Intl.DateTimeFormat([], {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    }).format(currentDate);
+  };
+
   return (
-    <div className="flex flex-col bg-background p-10">
-      {testMessages.map((testMessage: MessageProps) => {
-        console.log(testMessage);
+    <div className="flex w-[50%] flex-col bg-white p-10">
+      {testMessages.map((testMessage: MessageProps, index) => {
+        const macroTimestamp = getTimestamp(testMessages, index);
         return (
-          <MessageModule key={testMessage.message.id} message={testMessage} />
+          <MessageModule
+            key={testMessage.message.id}
+            message={testMessage}
+            timestamp={macroTimestamp}
+          />
         );
       })}
       <ChatBar />
