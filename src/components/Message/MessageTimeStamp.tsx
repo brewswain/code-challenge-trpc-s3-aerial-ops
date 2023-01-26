32e714b96b5b2130ff85de029c70eb7839825524
Brandon Lee <brandonlee2543@hotmail.com>
2023-01-26 19:41:06 -0400

    Created Naive timestamp Approach

--- src/components/Message/MessageTimeStamp.tsx
+++ src/components/Message/MessageTimeStamp.tsx
@@ -1,3 +1,5 @@
+import { timeStamp } from "console";
+import { useEffect, useState } from "react";
 import { MessageProps } from "../../data/testData";
 
 interface MessageTimeStampProps {
@@ -5,15 +7,48 @@ interface MessageTimeStampProps {
 }
 
 const MessageTimeStamp = ({ createdAt }: MessageTimeStampProps) => {
-  const timestamp = new Intl.DateTimeFormat([], {
-    hour: "numeric",
+  const [timeStamp, setTimeStamp] = useState<string>("");
+  // Detailed Timestamp block
+  const locale = "en-us";
+  const timestamp = new Intl.DateTimeFormat(locale, {
     minute: "numeric",
+    hour: "numeric",
     year: "numeric",
+    month: "short",
   }).format(createdAt);
+
+  // Using weekday in our Intl.DateTimeFormat was causing day to show up in weird place,
+  //  and I didn't want to use even more string manipulation
+  const summarizedDay = createdAt.toLocaleDateString(locale, {
+    weekday: "short",
+  });
+
+  const formattedTimestamp = summarizedDay + " " + timestamp.replace(",", " -");
+
+  // Minimal timestamp: "X minutes ago"
+  const previousMessageTimeInMinutes = Math.floor(
+    createdAt.getTime() / (1000 * 60)
+  );
+  const currentTimeinMinutes = Math.floor(Date.now() / (1000 * 60));
+  const timeSinceLastMessage =
+    currentTimeinMinutes - previousMessageTimeInMinutes;
+
+  // This current naive implementation will always show a stale timestamp since it'll just update once a new message
+  // is sent. However, the fix will be easier to conceive when I'm not relying on static data, thus it'll get
+  // focused on later. Maybe a periodic check via api, once a minute? seems expensive
+  useEffect(() => {
+    if (timeSinceLastMessage <= 5 && timeSinceLastMessage > 0) {
+      setTimeStamp(`${timeSinceLastMessage} minutes ago`);
+    } else if (timeSinceLastMessage > 5) {
+      setTimeStamp(formattedTimestamp);
+    } else {
+      setTimeStamp("sent just now");
+    }
+  }, [timestamp]);
 
   return (
     <div>
-      <p>{timestamp}</p>
+      <p>{timeStamp}</p>
     </div>
   );
 };
