a615eaf20e8eee95a5e241f6a808c51656dac55d
Brandon Lee <brandonlee2543@hotmail.com>
2023-01-26 10:55:59 -0400

    Fix imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { type AppType } from "next/app";
 
 import { api } from "../utils/api";
 
-import "../styles/globals.css";
+import "../styles/globals.scss";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
