ef19781dcfc36df42b623acd3a3a1f2492a57574
Brandon Lee <brandonlee2543@hotmail.com>
2023-01-26 14:19:29 -0400

    Added V1 of individual message's timestamp
    Still need to sort it properly, format is a little wonky

--- src/components/Message/MessageTimeStamp.tsx
+++ src/components/Message/MessageTimeStamp.tsx
@@ -1,5 +1,21 @@
-const MessageTimeStamp = () => {
-  return <div>time: skin past bone</div>;
+import { MessageProps } from "../../data/testData";
+
+interface MessageTimeStampProps {
+  createdAt: Date;
+}
+
+const MessageTimeStamp = ({ createdAt }: MessageTimeStampProps) => {
+  const timestamp = new Intl.DateTimeFormat([], {
+    hour: "numeric",
+    minute: "numeric",
+    year: "numeric",
+  }).format(createdAt);
+
+  return (
+    <div>
+      <p>{timestamp}</p>
+    </div>
+  );
 };
 
 export default MessageTimeStamp;
